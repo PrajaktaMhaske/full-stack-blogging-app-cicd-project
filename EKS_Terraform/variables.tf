01e2fbc1f0c1045bb8357bdab8328381d5527c65
PrajaktaMhaske <99183558+PrajaktaMhaske@users.noreply.github.com>
2024-11-13 20:15:25 +0530

    Update variables.tf
    updated  keypair name

--- EKS_Terraform/variables.tf
+++ EKS_Terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "ssh_key_name" {
   description = "The name of the SSH key pair to use for instances"
   type        = string
-  default     = "ubuntu-node-key-pair"
+  default     = "devops-keypair"
 }
